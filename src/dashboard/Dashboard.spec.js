227b4c05bb51a44dfcae65d3a101575edcbe783f
Brandon-Pampuch <brandonpampuch@gmail.com>
2019-06-19 20:51:58 -0500

    checks for all possible states of gate

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,18 @@
-// Test away
+import React from 'react'
+import * as rtl from '@testing-library/react'
+import renderer from 'react-test-renderer'
+import Dashboard from './Dashboard'
+
+//snapshot
+
+it('matches snapshot', () => {
+    const tree = renderer.create(<Dashboard />); // generates a DOM tree
+    // snapshots are a JSON representation of the DOM tree
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+
+
+
+
+
+
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,40 @@
-// Test away!
+import { render, fireEvent } from "@testing-library/react";
+import React from "react";
+import "@testing-library/react/cleanup-after-each"; // function at the top
+import "jest-dom/extend-expect"; // added functionality (disabled)
+import Display from "./Display";
+
+describe("display proper results", () => {
+  describe("describe proper colors", () => {
+    it("displayed and green", () => {
+        const display = render(<Display closed={false} locked={false}/>)
+        display.getByText('Open') 
+        display.getByText('Unlocked') 
+
+        expect(display.getByText('Open').className).toContain('green-led')
+
+        expect(display.getByText('Unlocked').className).toContain('green-led')
+
+    });
+    it("have a green unlock red closed", () => {
+        const display = render(<Display closed={true} locked={false}/>)
+        display.getByText('Closed') 
+        display.getByText('Unlocked') 
+
+        expect(display.getByText('Closed').className).toContain('red-led')
+
+        expect(display.getByText('Unlocked').className).toContain('green-led')
+
+    });
+    it("have a green unlock red closed", () => {
+        const display = render(<Display closed={true} locked={true}/>)
+        display.getByText('Closed') 
+        display.getByText('Locked') 
+
+        expect(display.getByText('Closed').className).toContain('red-led')
+
+        expect(display.getByText('Locked').className).toContain('red-led')
+
+    });
+  });
+});
